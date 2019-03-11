81004748a9ffeb9fb907e62bd1c1caad0ab79130
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-11 10:55:42 -0700

    second button added

--- project/project-click/index.js
+++ project/project-click/index.js
@@ -6,6 +6,7 @@ let currentColor = 0;
 let nextColor = 1;
 // we need a button to click
 const button = document.getElementsByTagName('button')[0];
+
 //we need to tell the button what to do when click it
 
 button.addEventListener('click',whenClicked);
@@ -18,15 +19,14 @@ function whenClicked(){
          }
     },1000);
 }
-// const button_1 = document.getElementById('bt-next')[1];
-// button_1.addEventListener('click',clickedNext);
-// function clickedNext(){
-//     setTimeout( () => {
-//          button_1.style.backgroundColor = colors[nextColor];
-//          nextColor++;
-//          if (nextColor >= colors.length){
-//           currentColor = 0;
-//         }
-//     },1000);
-// }
-
+const button_1 = document.getElementById('bt-next')[1];
+button_1.addEventListener('click',clickedNext);
+function clickedNext(){
+    setTimeout( () => {
+         button_1.style.backgroundColor = colors[nextColor];
+         nextColor++;
+         if (nextColor >= colors.length){
+          currentColor = 0;
+        }
+    },1000);
+}
